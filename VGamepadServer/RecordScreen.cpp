3f7e2408159787d364bff31621ff9dcafc7f49c8
Vuong Nguyen <vuongnq14798@gmail.com>
2020-04-04 21:18:17 +0700

    Encode data

--- VGamepadServer/RecordScreen.cpp
+++ VGamepadServer/RecordScreen.cpp
@@ -6,6 +6,7 @@
 extern "C"
 {
 #include "libavutil/opt.h"
+#include "libavutil/imgutils.h"
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
 #include "libswscale/swscale.h"
@@ -102,18 +103,18 @@ void CRecordScreen::capture()
 		static int height = 0;
 
 		HDC     hScreen = GetDC(NULL);
-		if (width == 0 || height == 0) 
+		if (width == 0 || height == 0)
 			Util::getResolution(width, height);
 		HDC     hDC = CreateCompatibleDC(hScreen);
 		HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, width, height);
 		HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
 		BOOL    bRet = BitBlt(hDC, 0, 0, width, height, hScreen, 0, 0, SRCCOPY | CAPTUREBLT);
 
-		//// save bitmap to clipboard
-		//OpenClipboard(NULL);
-		//EmptyClipboard();
-		//SetClipboardData(CF_BITMAP, hBitmap);
-		//CloseClipboard();
+		// save bitmap to clipboard
+		OpenClipboard(NULL);
+		EmptyClipboard();
+		SetClipboardData(CF_BITMAP, hBitmap);
+		CloseClipboard();
 
 		BITMAPINFOHEADER   bi;
 		bi.biSize = sizeof(BITMAPINFOHEADER);
@@ -156,5 +157,124 @@ void CRecordScreen::capture()
 
 int CRecordScreen::encodeImage()
 {
+	AVCodecContext* pCodecCtx;
+	AVCodec* pCodec;
+	AVPacket pkt;
+	uint8_t* picture_buf;
+	AVFrame* pFrame;
+	AVFrame* pFrameRGB;
+	int w_rgb, h_rgb;
+
+	int out_width = 352;
+	int out_height = 288;
+	AVInputFormat* iformat{};
+	AVOutputFormat* oformat{};
+
+	av_register_input_format(iformat);
+	av_register_output_format(oformat);
+
+	Util::getResolution(w_rgb, h_rgb);
+
+	pCodec = avcodec_find_encoder(AV_CODEC_ID_H264);
+	if (!pCodec) {
+		printf("Can not find encoder! \n");
+		return -1;
+	}
+
+	pCodecCtx = avcodec_alloc_context3(pCodec);
+	pCodecCtx->codec_id = AV_CODEC_ID_H264;
+	pCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
+	pCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+	pCodecCtx->width = out_width;
+	pCodecCtx->height = out_height;
+	pCodecCtx->time_base.num = 1;
+	pCodecCtx->time_base.den = 25;
+	pCodecCtx->bit_rate = 2000000;
+	pCodecCtx->gop_size = 10;
+	pCodecCtx->qmin = 2;
+	pCodecCtx->qmax = 10;
+
+	//Optional Param
+	pCodecCtx->max_b_frames = 3;
+	av_opt_set(pCodecCtx->priv_data, "preset", "slow", 0);
+
+	if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+		return -1;
+	}
+
+	pFrame = av_frame_alloc();
+	pFrame->format = AV_PIX_FMT_YUV420P;
+	pFrame->width = pCodecCtx->width;
+	pFrame->height = pCodecCtx->height;
+
+	pFrameRGB = av_frame_alloc();
+
+	int picture_size;
+	picture_size = av_image_get_buffer_size(pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, 32);
+	picture_buf = (uint8_t*)av_malloc(picture_size);
+
+	SwsContext* fooContext = sws_getContext(w_rgb, h_rgb, AV_PIX_FMT_RGB32, pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, NULL, NULL, NULL, NULL);
+	av_new_packet(&pkt, picture_size);
+	unsigned char* picture_buf_rgb = NULL;
+	unsigned int	picture_buff_size = 0;
+	while (TRUE)
+	{
+		if (!isRecording	//stop recording
+			&& pBufferRGB->getNumBuffer() == 0) //no buffer in queue
+			break;
+		do
+		{
+			if (pBufferRGB->getNumBuffer() == 0)
+			{
+				Sleep(1);
+			}
+			else
+			{
+				int qBufferCount = pBufferRGB->getMem(picture_buf_rgb, picture_buff_size);
+				if (pBufferRGB->getNumBuffer() > 0)
+				{
+
+					pBufferRGB->deleteMem(picture_buf_rgb);
+				}
+				else
+				{
+					break;
+				}
+			}
+		} while (encodingLoop);
+		// convert rgb to yuv
+		av_image_fill_arrays(pFrameRGB->data, pFrameRGB->linesize, picture_buf_rgb, AV_PIX_FMT_RGB32, w_rgb, h_rgb, 32);
+		av_image_fill_arrays(pFrame->data, pFrame->linesize, picture_buf, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, 32);
+		sws_scale(fooContext, pFrameRGB->data, pFrameRGB->linesize, 0, h_rgb, pFrame->data, pFrame->linesize);
+		pBufferRGB->deleteMem(picture_buf_rgb);
+
+		//Encode
+		
+		
+		int ret = avcodec_send_frame(pCodecCtx, pFrame);
+		if (ret < 0)
+		{
+			continue;
+		}
+		while (ret >= 0) {
+			ret = avcodec_receive_packet(pCodecCtx, &pkt);
+			if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+				int count = pBufferH264->setMem(pkt.data, pkt.size);
+				if (count > pBufferH264->getNumBufferMax())
+					pBufferH264->clearBuffer();
+				av_packet_unref(&pkt);
+			}
+			else if (ret < 0) {
+				break;
+			}
+		}
+	}
+
+	//Clean
+	avcodec_close(pCodecCtx);
+	av_free(pFrame);
+	av_free(pFrameRGB);
+	av_free(picture_buf);
+
 	return 1;
 }
